e2de664dd257857d995afb81139a0e8fb16659df
Anael-S <a.savoyant@gemvision.io>
2025-10-22 19:14:33 +0200

    Cleaned-up API DI + clean interface EpisodesRemoteDataSource to be able to mock-up test easily

--- app/src/main/java/com/anael/rickandmorty/data/repository/CharacterRepositoryImpl.kt
+++ app/src/main/java/com/anael/rickandmorty/data/repository/CharacterRepositoryImpl.kt
@@ -1,7 +1,7 @@
 package com.anael.rickandmorty.data.repository
 
 import com.anael.rickandmorty.data.mapper.toDomain
-import com.anael.rickandmorty.data.remote.RnMApiService
+import com.anael.rickandmorty.data.remote.EpisodesRemoteDataSource
 import com.anael.rickandmorty.data.utils.safeCall
 import com.anael.rickandmorty.domain.model.CharacterRnM
 import com.anael.rickandmorty.domain.repository.CharacterRepository
@@ -10,19 +10,19 @@ import javax.inject.Singleton
 
 @Singleton
 class CharacterRepositoryImpl @Inject constructor(
-    private val apiService: RnMApiService,
+    private val remote: EpisodesRemoteDataSource,
 ) : CharacterRepository {
 
     override suspend fun getCharacter(id: String): Result<CharacterRnM> =
-        safeCall { apiService.getCharacterById(id) }
+        safeCall { remote.getCharacterById(id) }
             .map { dto -> dto.toDomain() }
 
     override suspend fun getCharactersByIds(ids: List<String>): Result<List<CharacterRnM>> =
         safeCall {
             when (ids.size) {
                 0 -> emptyList()
-                1 -> listOf(apiService.getCharacterById(ids.first()))
-                else -> apiService.getCharactersByIds(ids.joinToString(","))
+                1 -> listOf(remote.getCharacterById(ids.first()))
+                else -> remote.getCharactersByIds(ids.joinToString(","))
             }
         }.map { list -> list.map { it.toDomain() } }
 }
